0ba4190133e478d705f40bf7ef6767937846d67f
cz <chenze.3057@bytedance.com>
2021-10-27 21:40:22 +0800

    feat: add trash & tostring in foreign_seri

--- 3rd/numsky/src/lua-foreign_seri.c
+++ 3rd/numsky/src/lua-foreign_seri.c
@@ -451,6 +451,43 @@ static int lremoteunpack(lua_State *L) {
 	return foreign_unpack(L, MODE_FOREIGN_REMOTE);
 }
 
+static int ltostring(lua_State *L) {
+	int t = lua_type(L,1);
+	switch (t) {
+	case LUA_TSTRING: {
+		lua_settop(L, 1);
+		return 1;
+	}
+	case LUA_TLIGHTUSERDATA: {
+		char * msg = (char*)lua_touserdata(L,1);
+		int sz = luaL_checkinteger(L,2);
+		lua_pushlstring(L,msg,sz);
+		return 1;
+	}
+	default:
+		return 0;
+	}
+}
+
+static int ltrash(lua_State *L) {
+	int t = lua_type(L,1);
+	switch (t) {
+	case LUA_TSTRING: {
+		break;
+	}
+	case LUA_TLIGHTUSERDATA: {
+		void * msg = lua_touserdata(L,1);
+		luaL_checkinteger(L,2);
+		skynet_free(msg);
+		break;
+	}
+	default:
+		luaL_error(L, "skynet.trash invalid param %s", lua_typename(L,t));
+	}
+
+	return 0;
+}
+
 static const struct luaL_Reg l_methods[] = {
     { "luapack" , lluapack },
     { "luaunpack", lluaunpack },
@@ -458,6 +495,8 @@ static const struct luaL_Reg l_methods[] = {
     { "unpack" , lunpack },
     { "remotepack", lremotepack },
     { "remoteunpack", lremoteunpack },
+    { "tostring", ltostring },
+    { "trash", ltrash },
     { NULL,  NULL },
 };
 
